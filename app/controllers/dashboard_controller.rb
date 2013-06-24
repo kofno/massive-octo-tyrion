50d399b45159a880a5e63ac663995dd1ff7917fe
Ryan L. Bell <ryan.l.bell@gmail.com>
2013-06-24 16:51:54 -0400

    Etag support for dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,7 @@ class DashboardController < ApplicationController
   def view
     @user = User.find(params[:user_id])
     @accounts = Account.find(params[:user_id])
+    fresh_when [@user, @accounts]
   end
 
 end
